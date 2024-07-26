4a6c10860f16ddb5a3605d6498f934f0fba4ccfc
Tban <horlaryinka0@gmail.com>
2024-07-26 10:42:43 +0100

    Third comit

--- src/components/lastElement/LE.jsx
+++ src/components/lastElement/LE.jsx
@@ -4,10 +4,11 @@ import './LE.scss';
 
 export default function LE() {
   return (
-    
+  
     <div className='element'>
       <div className='img'><img src="Images/mobile.png" alt="Mobile" /></div>
     </div>
+  
     
 
     
