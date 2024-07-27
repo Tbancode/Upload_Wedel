5b4d7115773dacb5f1ad28fb108a132ea0e9711f
Tban <horlaryinka0@gmail.com>
2024-07-27 14:10:39 +0100

    Fourth commit

--- src/components/lastElement/LE.jsx
+++ src/components/lastElement/LE.jsx
@@ -6,7 +6,7 @@ export default function LE() {
   return (
     
     <div className='element'>
-      <div className='img'><img src="Images/mobileW.png" alt="Mobile" /></div>
+      <div className='img'><img src="Images/webV.png" alt="Mobile" /></div>
       <div className='imgP'><img src="Images/mobilePHV.png" alt="Mobile" /></div>
     </div>
     
